7abd74148adb7b279167877b315a3c91e45603b6
agarwalayush9 <ayushag.cse@gmail.com>
2024-07-03 16:54:38 +0530

    initial commit

---
+++ Shlves-Library/Shlves_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shlves_LibraryApp.swift
+//  Shlves-Library
+//
+//  Created by Ayush Agarwal on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Shlves_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
