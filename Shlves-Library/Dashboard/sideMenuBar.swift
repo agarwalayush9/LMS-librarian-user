beff46e1642c1b134325cc792434603ab8c27e65
ZekDRACULA <84472796+zekDRACULA@users.noreply.github.com>
2024-07-04 23:48:27 +0530

    added Buttons

--- Shlves-Library/Dashboard/sideMenuBar.swift
+++ Shlves-Library/Dashboard/sideMenuBar.swift
@@ -7,38 +7,28 @@
 
 import SwiftUI
 
-struct sideMenuBar: View {
-    @Binding var isShowing : Bool
-    
+
+struct sideMenu : View {
     var body: some View {
         ZStack{
-            if isShowing{
-                Rectangle()
-                    .opacity(0.3)
-                    .ignoresSafeArea()
-                    .background(ignoresSafeAreaEdges: .all)
-                    .onTapGesture {
-                        isShowing.toggle()
-                    }
-                        HStack{
-                            VStack(alignment: .leading){
-                                SideBarOptions()
-                                
-                                Spacer()
-                            }
-                            .background(Color.white)
-                            .frame(width: 400)
-                            .offset(x: isShowing ? 0 : -300)
-                            .animation(.easeInOut(duration: 0.3),
-                                       value: isShowing)
-                            Spacer()
-                        }
-            }
+            
         }
-        .transition(.move(edge: .leading))
-        .animation(.easeInOut, value: isShowing)
-    }}
-
-#Preview {
-    sideMenuBar(isShowing: .constant(true))
+    }
 }
+
+struct sideMenuBar: View {
+    var body: some View {
+        ZStack{
+            Button(action: {
+                //Menu
+            }, label: {
+                Text("Open Menu")
+                    .frame(width: 200, height: 50, alignment: .center)
+            })
+        }
+    }
+}
+
+//#Preview {
+//   // sideMenuBar(isShowing: .constant(true))
+//}
