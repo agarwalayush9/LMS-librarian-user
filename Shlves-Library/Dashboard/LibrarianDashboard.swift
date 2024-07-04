8004e60e2b9ae04ee01e2d697c19e69a20279e82
ZekDRACULA <84472796+zekDRACULA@users.noreply.github.com>
2024-07-04 15:16:51 +0530

    navigation Bar Seperator has been updated

--- Shlves-Library/Dashboard/LibrarianDashboard.swift
+++ Shlves-Library/Dashboard/LibrarianDashboard.swift
@@ -8,181 +8,79 @@
 import SwiftUI
 
 struct LibrarianDashboard: View {
-    
-    let data = Analytics.analytics
-    
     var body: some View {
-        ZStack{
-            HStack(spacing : 0){
-                VStack(alignment: .leading, spacing: 16){
-                    userName(userName: "Ankit")
-                    todayDateAndTime()
-                    
+        
+        NavigationStack{
+            ZStack{
+            backgroundView()
+            VStack{
+                HStack(spacing : 0){
+                    VStack(alignment: .leading, spacing: 16){
+                        userName(userName: "User")
+                        todayDateAndTime()
+                        
+                    }
+                    .padding(.all, 64)
+                    card(title: "Today’s Revenue",
+                         value: 221, salesDifferencePercentage: 2.5)
                 }
-                .padding([.leading, .top, .bottom], 64)
-                card(title: "Today’s Revenue",
-                     value: 221, salesDifferencePercentage: 2.5)
-            }
-        }
-        //.background(.gray)
-        .padding(.trailing, 462)
-        VStack{
-        VStack(alignment: .leading, spacing: 20){
-            AnalyticHeader(title: "Main Analtics Below")
-            HStack{
-                ForEach(data){ datunm in
-                    card(title: datunm.title,
-                         value: datunm.value,
-                         salesDifferencePercentage: datunm.salesDifferencePercentage)
-                }
-            }
-            .padding(.leading,64)
-        }
-        //.padding(.bo)
-            VStack(alignment: .leading, spacing: 20){
-                AnalyticHeader(title: "Main Analtics Below")
-                HStack{
-                    ForEach(data){ datunm in
-                        card(title: datunm.title,
-                             value: datunm.value,
-                             salesDifferencePercentage: datunm.salesDifferencePercentage)
+                .padding(.trailing, 462)
+                ScrollView{
+                    VStack{
+                        //inside this write BookCircilation
+                        VStack(alignment: .leading,spacing: 20){
+                            AnalyticHeader(title: "Main Analytics Below")
+                            ScrollView{
+                                HStack(spacing: 20){
+                                    DashboardAnalytics()
+                                }
+                                .padding(.leading,64)
+                            }
+                            VStack(alignment: .leading, spacing: 20){
+                                AnalyticHeader(title: "Main Analytics Below")
+                                ScrollView{
+                                    HStack(spacing: 20){
+                                        DashboardAnalytics()
+                                    }
+                                    .padding(.leading,64)
+                                }
+                                //here
+                            }
+                            .padding([.bottom], 16)
+                            Spacer()
+                        }
+                        .padding([.top,.bottom], 16)
+                        Spacer()
                     }
                 }
-                .padding(.leading,64)
-                Spacer()
-            }
-    }
-    }
-        
-}
-
-struct userName : View {
-    var userName : String
-    var body: some View {
-            Text("Hello, \(userName)!")
-                .font(
-                  Font.custom("DM Sans", size: 48)
-                    .weight(.medium)
-                )
-                .foregroundColor(.black)
-                .frame(maxWidth: .infinity, alignment: .topLeading)
-    }
-}
-
-struct card : View {
-    var title : String
-    var value : Double
-    var salesDifferencePercentage : Double
-    var body: some View {
-        Rectangle()
-            .foregroundStyle(.white)
-            .frame(width: 258, height: 160)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .overlay(
-               cardData(title: title,
-                        value: value,
-                        salesDifferencePercentage: salesDifferencePercentage)
-            )
-    }
-}
-
-
-struct DashboardAnalytics : View {
-    let data = Analytics.analytics
-    var body: some View {
-        ForEach(data){ datunm in
-            card(title: datunm.title,
-                 value: datunm.value,
-                 salesDifferencePercentage: datunm.salesDifferencePercentage)
-        }
-    }
-}
-
-struct cardData : View {
-    
-    var title : String
-    var value : Double
-    var salesDifferencePercentage : Double
-    
-    var body: some View {
-        HStack {
-            VStack {
-                Text("$\(String(format: "%.2f", value))")
-                  .font(
-                    Font.custom("DM Sans", size: 32)
-                      .weight(.bold)
-                  )
-                  .padding(.leading, 19)
-                  .padding()
-                Text(title)
-                  .font(
-                    Font.custom("DM Sans", size: 16)
-                      .weight(.medium)
-                  )
-                  .padding(.leading, 19)
-                  
-            }
-            .foregroundColor(Color(red: 0.16, green: 0.14, blue: 0.14))
-            .frame(maxWidth: .infinity, alignment: .topLeading)
-            
-            
-            VStack {
                 
-                Text("\(String(format: "%.2f", salesDifferencePercentage))%")
-                  .font(
-                    Font.custom("DM Sans", size: 12)
-                      .weight(.medium)
-                  )
-                  .foregroundColor(Color(red: 0, green: 0.74, blue: 0.35))
-              .padding(.trailing)
             }
         }
+            .navigationTitle("LMS")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .toolbar{
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "sidebar.left")
+                            .foregroundStyle(Color.black)
+                            
+                    })
+                }
+            }
     }
+ }
 }
 
-
-struct todayDateAndTime : View {
+struct backgroundView : View {
     var body: some View {
-        Text(currentDateAndTime())
-          .font(
-            Font.custom("DM Sans", size: 24)
-              .weight(.medium)
-          )
-          .foregroundColor(.black)
-          .frame(maxWidth: .infinity, alignment: .topLeading)
+        Color("dashboardbg").ignoresSafeArea()
+        
     }
 }
 
-struct AnalyticHeader : View {
-    var title : String
-    var body: some View {
-        Text(title)
-          .font(
-            Font.custom("DM Sans", size: 20)
-              .weight(.medium)
-          )
-          .foregroundColor(.black)
-          .frame(maxWidth: .infinity, alignment: .topLeading)
-          .padding([.leading], 64)
-    }
-}
-
-
-func currentDateAndTime() -> String {
-        let now = Date()
-        
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "d MMMM, yyyy | EEEE"
-        let dateString = dateFormatter.string(from: now)
-        
-        
-        let timeFormatter = DateFormatter()
-        timeFormatter.dateFormat = "h:mm a"
-        let timeString = timeFormatter.string(from: now)
-        
-        return "\(dateString), \(timeString)"
-    }
 
 #Preview {
     LibrarianDashboard()
