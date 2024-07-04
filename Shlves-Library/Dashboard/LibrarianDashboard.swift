0bcaa1adbc160f0739473ddde6b37c86a9ae06bd
ZekDRACULA <84472796+zekDRACULA@users.noreply.github.com>
2024-07-04 06:04:42 +0530

    Analytics is made
    refactoring of code is needed

--- Shlves-Library/Dashboard/LibrarianDashboard.swift
+++ Shlves-Library/Dashboard/LibrarianDashboard.swift
@@ -2,48 +2,176 @@
 //  LibrarianDashboard.swift
 //  Shlves-Library
 //
-//  Created by Abhay singh on 03/07/24.
+//  Created by Abhay singh on 04/07/24.
 //
 
 import SwiftUI
 
 struct LibrarianDashboard: View {
-    @State private var showMenu = false
+    
+    let data = Analytics.analytics
     
     var body: some View {
-        NavigationStack {
-            ZStack{
-                VStack{
-                    Text("Hello, Ankit!")
-                      .font(
-                        Font.custom("DM Sans", size: 48)
-                          .weight(.medium)
-                      )
-                      .foregroundColor(.black)
-                      .frame(maxWidth: .infinity, alignment: .topLeading)
-                
+        ZStack{
+            HStack(spacing : 0){
+                VStack(alignment: .leading, spacing: 16){
+                    userName(userName: "Ankit")
+                    todayDateAndTime()
+                    
+                }
+                .padding([.leading, .top, .bottom], 64)
+                card(title: "Today’s Revenue",
+                     value: 221, salesDifferencePercentage: 2.5)
             }
-                
-                sideMenuBar(isShowing: $showMenu)
         }
-            .toolbar(showMenu ? .hidden : .visible,
-                     for: .navigationBar)
-            .navigationTitle("Khvaab Library")
-            .navigationBarTitleDisplayMode(.inline)
-            .padding()
-            .toolbar{
-                ToolbarItem(placement: .topBarLeading){
-                    Button(action: {
-                        showMenu.toggle()
-                    }, label: {
-                        Image(systemName: "sidebar.left")
-                    })
+        //.background(.gray)
+        .padding(.trailing, 462)
+        VStack{
+        VStack(alignment: .leading, spacing: 20){
+            AnalyticHeader(title: "Main Analtics Below")
+            HStack{
+                ForEach(data){ datunm in
+                    card(title: datunm.title,
+                         value: datunm.value,
+                         salesDifferencePercentage: datunm.salesDifferencePercentage)
                 }
             }
+            .padding(.leading,64)
         }
-        .background(ignoresSafeAreaEdges: .all)
+        .padding(.bottom, 13)
+            VStack(alignment: .leading, spacing: 20){
+                AnalyticHeader(title: "Main Analtics Below")
+                HStack{
+                    ForEach(data){ datunm in
+                        card(title: datunm.title,
+                             value: datunm.value,
+                             salesDifferencePercentage: datunm.salesDifferencePercentage)
+                    }
+                }
+                .padding(.leading,64)
+                Spacer()
+            }
+    }
+    }
+        
+}
+
+struct userName : View {
+    var userName : String
+    var body: some View {
+            Text("Hello, \(userName)!")
+                .font(
+                  Font.custom("DM Sans", size: 48)
+                    .weight(.medium)
+                )
+                .foregroundColor(.black)
+                .frame(maxWidth: .infinity, alignment: .topLeading)
     }
 }
+
+struct card : View {
+    var title : String
+    var value : Double
+    var salesDifferencePercentage : Double
+    var body: some View {
+        Rectangle()
+            .foregroundStyle(Color("dashboardbg"))
+            .frame(width: 258, height: 160)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .overlay(
+               cardData(title: title,
+                        value: value,
+                        salesDifferencePercentage: salesDifferencePercentage)
+            )
+    }
+}
+
+
+struct cardData : View {
+    
+    var title : String
+    var value : Double
+    var salesDifferencePercentage : Double
+    
+    var body: some View {
+        HStack {
+            VStack {
+                Text("$\(String(format: "%.2f", value))")
+                  .font(
+                    Font.custom("DM Sans", size: 32)
+                      .weight(.bold)
+                  )
+                  .padding(.leading, 19)
+                  .padding()
+                Text(title)
+                  .font(
+                    Font.custom("DM Sans", size: 16)
+                      .weight(.medium)
+                  )
+                  .padding(.leading, 19)
+                  
+            }
+            .foregroundColor(Color(red: 0.16, green: 0.14, blue: 0.14))
+            .frame(maxWidth: .infinity, alignment: .topLeading)
+            
+            
+            VStack {
+                
+                Text("\(String(format: "%.2f", salesDifferencePercentage))%")
+                  .font(
+                    Font.custom("DM Sans", size: 12)
+                      .weight(.medium)
+                  )
+                  .foregroundColor(Color(red: 0, green: 0.74, blue: 0.35))
+              .padding(.trailing)
+            }
+        }
+    }
+}
+
+
+struct todayDateAndTime : View {
+    var body: some View {
+        Text(currentDateAndTime())
+          .font(
+            Font.custom("DM Sans", size: 24)
+              .weight(.medium)
+          )
+          .foregroundColor(.black)
+          .frame(maxWidth: .infinity, alignment: .topLeading)
+    }
+}
+
+struct AnalyticHeader : View {
+    var title : String
+    var body: some View {
+        Text(title)
+          .font(
+            Font.custom("DM Sans", size: 20)
+              .weight(.medium)
+          )
+          .foregroundColor(.black)
+          .frame(maxWidth: .infinity, alignment: .topLeading)
+          .padding([.leading], 64)
+    }
+}
+
+
+func currentDateAndTime() -> String {
+        let now = Date()
+        
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d MMMM, yyyy | EEEE"
+        let dateString = dateFormatter.string(from: now)
+        
+        
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = "h:mm a"
+        let timeString = timeFormatter.string(from: now)
+        
+        return "\(dateString), \(timeString)"
+    }
 
 #Preview {
     LibrarianDashboard()
