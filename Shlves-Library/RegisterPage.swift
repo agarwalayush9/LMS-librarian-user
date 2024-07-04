7e1aa484183f6ba341fef9627236abf2a54a2577
Aanchal Saxena <aanchalsaxenacontact@gmail.com>
2024-07-04 23:50:32 +0530

    book catalogue ui

--- Shlves-Library/RegisterPage.swift
+++ Shlves-Library/RegisterPage.swift
@@ -5,7 +5,6 @@
 //  Created by Ayush Agarwal on 04/07/24.
 //
 
-import Foundation
 import SwiftUI
 import FirebaseAuth
 import FirebaseDatabase
@@ -18,118 +17,106 @@ struct RegisterPage: View {
     @State private var phoneNumber: String = ""
     @State private var showAlert = false
     @State private var alertMessage = ""
+    @State private var isRegistrationSuccessful = false
 
     var body: some View {
-        HStack {
-            // Left side image
-            Image("librarian")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            
-            // Right side form
-            VStack(alignment: .leading, spacing: 20) {
-                // Logo and title
-                HStack {
-                    Image("Logo")
-                        .resizable()
-                        .frame(width: 36.75, height: 37.5)
-                    Text("Shelves")
-                        .font(.largeTitle)
+        NavigationStack {
+            HStack {
+                // Left side image
+                Image("librarian")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                // Right side form
+                VStack(alignment: .leading, spacing: 20) {
+                    // Logo and title
+                    HStack {
+                        Image("Logo")
+                            .resizable()
+                            .frame(width: 36.75, height: 37.5)
+                        Text("Shelves")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(red: 0.4, green: 0.2, blue: 0.1)) // Assuming LogoColor is defined in Assets
+                    }
+                    
+                    Text("Librarian Registration")
+                        .font(.title)
                         .fontWeight(.bold)
-                        .foregroundColor(Color(red:0.4,green:0.2,blue:0.1)) // Assuming LogoColor is defined in Assets
-                }
-                
-                Text("Librarian Registration")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color(red:0.4,green:0.2,blue:0.1)) // Assuming TitleColor is defined in Assets
-                
-                Text("Please enter your details.")
-                    .foregroundColor(Color(red:0.4,green:0.2,blue:0.1))
-                
-                // Name field
-                
-                Text("Name")
-                    .foregroundColor(Color.gray)
-                TextField("Enter your name", text: $name)
-                    .padding()
-                    .background(Color(UIColor.systemGray6))
-                    .cornerRadius(8)
-                
-                // Email field
-                
+                        .foregroundColor(Color(red: 0.4, green: 0.2, blue: 0.1)) // Assuming TitleColor is defined in Assets
+                    
+                    Text("Please enter your details.")
+                        .foregroundColor(Color(red: 0.4, green: 0.2, blue: 0.1))
+                    
+                    // Name field
+                    Text("Name")
+                        .foregroundColor(Color.gray)
+                    TextField("Enter your name", text: $name)
+                        .padding()
+                        .background(Color(UIColor.systemGray6))
+                        .cornerRadius(8)
+                    
+                    // Email field
                     Text("Email")
                         .foregroundColor(Color.gray)
                     TextField("Enter your email", text: $email)
                         .padding()
                         .background(Color(UIColor.systemGray6))
                         .cornerRadius(8)
-                
-                
-                // Password field
-                
+                    
+                    // Phone Number field
                     Text("Phone Number")
                         .foregroundColor(Color.gray)
                     TextField("Enter your number", text: $phoneNumber)
                         .padding()
                         .background(Color(UIColor.systemGray6))
-                        .cornerRadius(8).padding(.bottom, 15)
-                
-                
-                Button(action: {
-                    phoneNumber = ""
-                    email = ""
-                    name = ""
+                        .cornerRadius(8)
+                        .padding(.bottom, 15)
+                        .keyboardType(.numberPad)
                     
-                    guard !name.isEmpty, !email.isEmpty, let phoneNumber = Int(phoneNumber) else {
-                    alertMessage = "Please fill in all fields correctly."
-                    showAlert = true
-                        return
+                    NavigationLink(destination: LibrarianDashboard(), isActive: $isRegistrationSuccessful) {
+                        EmptyView()
                     }
-                                
-                    let user = User(name: name, email: email, phoneNumber: phoneNumber)
-                    DataController.shared.addUser(user) { result in
-                        switch result {
+                    
+                    Button(action: {
+                        guard !name.isEmpty, !email.isEmpty, let phoneNumber = Int(phoneNumber) else {
+                            alertMessage = "Please fill in all fields correctly."
+                            showAlert = true
+                            return
+                        }
+                        
+                        let user = User(name: name, email: email, phoneNumber: phoneNumber)
+                        DataController.shared.addUser(user) { result in
+                            switch result {
                             case .success:
-                                alertMessage = "Registration successful!"
+                                print("Registration successful!")
+                                isRegistrationSuccessful = true
                             case .failure(let error):
                                 alertMessage = error.localizedDescription
-                            }
                                 showAlert = true
-                                
+                            }
+                            
+                        }
+                    }) {
+                        Text("Register")
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color(red: 0.4, green: 0.2, blue: 0.1)) // Assuming ButtonColor is defined in Assets
+                            .cornerRadius(8)
+                    }
+                    .alert(isPresented: $showAlert) {
+                        Alert(title: Text("Registration"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                    }
                 }
-                    
-                }) {
-                    Text("Register")
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color(red:0.4,green:0.2,blue:0.1)) // Assuming ButtonColor is defined in Assets
-                        .cornerRadius(8)
-                }
-                
-                .alert(isPresented: $showAlert) {
-                Alert(title: Text("Registration"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-                }
-
-                
-                
-                
+                .padding()
+                .frame(maxWidth: .infinity, alignment: .leading)
             }
             .padding()
-            .frame(maxWidth: .infinity, alignment: .leading)
         }
-        .padding()
     }
-    
-
-    
 }
-
-
-
-
 
 #Preview {
     RegisterPage()
