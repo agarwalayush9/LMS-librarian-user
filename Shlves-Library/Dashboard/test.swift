2ddbfbcd0a8d0cec4ce95f16339f5677683a7b65
ZekDRACULA <84472796+zekDRACULA@users.noreply.github.com>
2024-07-06 15:16:52 +0530

    testing

---
+++ Shlves-Library/Dashboard/test.swift
@@ -0,0 +1,45 @@
+//
+//  test.swift
+//  Shlves-Library
+//
+//  Created by Abhay singh on 06/07/24.
+//
+
+import SwiftUI
+
+struct test : View {
+    var body: some View {
+        NavigationStack{
+            ZStack{
+                
+            }
+            .navigationTitle("Test")
+            .navigationBarTitleDisplayMode(.inline)
+            Divider()
+            Spacer()
+        }
+    }
+}
+
+
+
+struct MenuContent : View{
+    var body : some View{
+        ZStack{
+            
+        }
+    }
+}
+
+struct SideMenu : View{
+    var body : some View{
+        ZStack{
+            
+        }
+    }
+}
+
+
+#Preview {
+    test()
+}
