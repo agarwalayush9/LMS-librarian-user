ea29ef36774e2af30d37cce4e6dedd903eb728ac
ZekDRACULA <84472796+zekDRACULA@users.noreply.github.com>
2024-07-11 11:16:25 +0530

    made skeleton for Event dashboard
    aligning "Manage events "is left

--- Shlves-Library/Dashboard/EventsDashboard.swift
+++ Shlves-Library/Dashboard/EventsDashboard.swift
@@ -12,16 +12,99 @@ struct EventsDashboard: View {
     var body: some View {
         NavigationStack {
             ZStack{
-                VStack(alignment : .leading){
-                    Text("Manage Events")
-                        .font(
-                        Font.custom("DM Sans", size: 52)
-                        .weight(.medium)
-                        )
-                        
-                        customGraphCard(width: 730,
-                                        height: 259)
-                    Spacer()
+                VStack{
+                    HStack{
+                        ScrollView {
+                            VStack{
+                                Text("Manage Events")
+                                    .font(
+                                    Font.custom("DM Sans", size: 52)
+                                    .weight(.medium)
+                                    )
+                                    .frame(alignment: .leading)
+                                    //Event Revenue Details Card
+                                    customGraphCard(width: 730,
+                                                    height: 250)
+                                    .frame(alignment: .topLeading)
+                                
+                                //MARK: Line 2
+                                HStack{
+                                    // Total Event Visitors Card
+                                    customGraphCard(width: 437,
+                                                    height: 243)
+                                    .padding(.trailing)
+                                    //Today's Event Card
+                                    customGraphCard(width: 270,
+                                                    height: 243)
+                                    
+                                }
+                                //MARK: Line 3
+                                HStack{
+                                    //Tickets status Card
+                                    customGraphCard(width: 270,
+                                                    height: 243)
+                                    .padding(.trailing)
+                                    // Total Event Visitors Card
+                                    customGraphCard(width: 437,
+                                                    height: 243)
+                                }
+                                
+                                Spacer()
+                            }
+                                .padding(.trailing, 64)
+                                
+                        }
+                        //MARK: Right of Manage event
+                        ScrollView {
+                            VStack(alignment : .leading){
+                                //Calender Card
+                                customGraphCard(width: 344 ,
+                                                height: 285)
+                                .padding([.leading, .trailing])
+                                HStack(alignment: .center){
+                                    Text("Upcoming Events")
+                                        .font(
+                                            Font.custom("DM sans", size: 36)
+                                                .weight(.bold)
+                                        )
+                                    Text("See All")
+                                        .frame(alignment: .bottom)
+                                        .font(
+                                            Font.custom("DM sans", size: 14)
+                                                .weight(.medium)
+                                        ).foregroundStyle(Color("CustomButtonColor"))
+                                }.padding([.leading, .trailing])
+                                
+                                //MARK: beneath will be two closest upcoming events only 2
+                                
+                                //Probably i'll make this a list
+                                Text("Tomorrow")
+                                    .font(
+                                        Font.custom("DM sans", size: 16)
+                                            .weight(.bold)
+                                    )
+                                    .padding(.top, 1)
+                                    .padding([.leading, .trailing])
+                                customGraphCard(width: 344, height: 194)
+                                    .padding([.leading, .trailing])
+                                
+                                Text("15 JULY")
+                                    .font(
+                                        Font.custom("DM sans", size: 16)
+                                            .weight(.bold)
+                                    ).padding(.top, 22)
+                                    .padding([.leading, .trailing])
+                                
+                                customGraphCard(width: 344, height: 194)
+                                    .padding([.leading, .trailing])
+                                
+                                //till here
+                                
+                            }
+                        }.padding([.leading, .trailing], 30)
+                            Spacer()
+                    }
+                    
                 }
                 .padding([.top, .leading], 64)
                 .frame(width: UIScreen.main.bounds.width, alignment: .leading)
@@ -93,10 +176,10 @@ struct customGraphCard: View {
     var width : Double
     var height : Double
     
-    
     var body: some View {
         Rectangle()
             .frame(width: width, height: height)
-            .background(.blue)
+            .foregroundStyle(Color(.yellow))
+            .clipShape(RoundedRectangle(cornerRadius: 14))
     }
 }
