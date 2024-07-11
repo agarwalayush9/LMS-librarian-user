0a3b0503d25c8d70eb6cad06bf3a5f4388caffeb
ZekDRACULA <84472796+zekDRACULA@users.noreply.github.com>
2024-07-11 21:34:23 +0530

    fully made skeleton for event Management

--- Shlves-Library/Dashboard/EventsDashboard.swift
+++ Shlves-Library/Dashboard/EventsDashboard.swift
@@ -12,7 +12,7 @@ struct EventsDashboard: View {
     var body: some View {
         NavigationStack {
             ZStack{
-                EventAnalyticsDashboard()
+                EventAnalyticsCard()
                 //This is to be the last part of z Stack
                 if menuOpened {
                     sideMenu(isLoggedIn: .constant(true), width: UIScreen.main.bounds.width * 0.30,
@@ -72,7 +72,6 @@ func textColorChanger(title : String) -> Text{
 struct customGraphCard: View {
     var width : Double
     var height : Double
-    
     var body: some View {
         Rectangle()
             .frame(width: width, height: height)
@@ -81,125 +80,175 @@ struct customGraphCard: View {
     }
 }
 
-struct EventAnalyticsDashboard: View {
+struct customEventCard: View {
+    var width : Double
+    var height : Double
+//    var imageName : CIImage
+//    var eventName : String
+//    var eventDate : String
+//    var eventLocation : String
+//    var hostName : String
+    
     var body: some View {
-        VStack{
-            HStack{
-                ScrollView {
+        Rectangle()
+            .frame(width: width, height: height)
+            .foregroundStyle(Color(.yellow))
+            .clipShape(RoundedRectangle(cornerRadius: 14))
+            .overlay(
+                HStack{
+                    Image("book_cover")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 112, height: 150)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+    
                     VStack(alignment: .leading){
-                        Text("Manage Events")
+                        //event name
+                        Text("Event Name:")
                             .font(
-                                Font.custom("DMSans-Medium", size: 52)
+                                Font.custom("DMSansBold", size: 12)
                             )
+                            .foregroundStyle(.gray)
                         
-                        //Event Revenue Details Card
-                        customGraphCard(width: 740,
-                                        height: 250)
-                        
-                        
-                        //MARK: Line 2
+                        Text("California Art Festival 2023 Dana Point 29-30")
+                            .font(
+                            Font.custom("DM Sans", size: 16)
+                            .weight(.bold)
+                            )
+                            .frame(maxWidth: 150,maxHeight: 150)
+//                            .padding(.leading,45)
+                            .lineLimit(nil)
+                            .fixedSize(horizontal: false, vertical: true)
+                            .multilineTextAlignment(.leading)
+                        //Event Details
+                        Text("Event Details:")
+                            .foregroundStyle(.gray)
                         HStack{
-                            // Total Event Visitors Card
-                            customGraphCard(width: 442,
-                                            height: 243)
-                            .padding(.trailing)
-                            //Today's Event Card
-                            customGraphCard(width: 275,
-                                            height: 243)
-                            
+                            Image(systemName: "calendar")
+                            Text("11 JULY 2024")
                         }
-                        //MARK: Line 3
                         HStack{
-                            //Tickets status Card
-                            customGraphCard(width: 275,
-                                            height: 243)
-                            .padding(.trailing)
-                            
-                            // Total Event Visitors Card
-                            customGraphCard(width: 442,
-                                            height: 243)
+                            Image(systemName: "mappin")
+                            Text("Shelves Library")
                         }
-                        
-                        Spacer()
-                    }
-                    .padding(.trailing, 54)
-                    
-                }
-                //MARK: Right of Manage event
-                ScrollView {
-                    VStack(alignment : .leading){
-                        //Calender Card
-                        customGraphCard(width: 344 ,
-                                        height: 285)
-                        .padding([.leading, .trailing])
-                        HStack{
-                            Text("Upcoming Events")
+                        Text("Host Name")
+                            .foregroundStyle(.gray)
+                        Text("Kaleem Bhaiya")
+                    }.padding(.leading, 15)
+                    .font(
+                        Font.custom("DM Sans", size: 16)
+                        .weight(.bold)
+                        )
+                    .padding(.trailing, 45)//End of VStack for Event Details
+                }//End of HStack
+            )
+    }
+}
+
+struct EventAnalyticsCard: View {
+    var body: some View {
+        ScrollView {
+            VStack{
+                HStack{
+                    ScrollView {
+                        VStack(alignment: .leading){
+                            Text("Manage Events")
                                 .font(
-                                    Font.custom("DMSansBold", size: 36)
+                                    Font.custom("DMSans-Medium", size: 52)
                                 )
-                            Text("See All")
-                                .frame(alignment: .bottom)
-                                .font(
-                                    Font.custom("DMSans-Medium", size: 14)
-                                ).foregroundStyle(Color("CustomButtonColor"))
-                        }.padding([.leading, .trailing])
+                            
+                            //Event Revenue Details Card
+                            customGraphCard(width: 740,
+                                            height: 250)
+                            
+                            
+                            //MARK: Line 2
+                            HStack{
+                                // Total Event Visitors Card
+                                customGraphCard(width: 442,
+                                                height: 243)
+                                .padding(.trailing)
+                                //Today's Event Card
+                                customGraphCard(width: 275,
+                                                height: 243)
+                                
+                            }
+                            //MARK: Line 3
+                            HStack{
+                                //Tickets status Card
+                                customGraphCard(width: 275,
+                                                height: 243)
+                                .padding(.trailing)
+                                
+                                // Total Event Visitors Card
+                                customGraphCard(width: 442,
+                                                height: 243)
+                            }
+                            
+                            Spacer()
+                        }
+                        .padding(.trailing, 54)
                         
-                        //MARK: beneath will be two closest upcoming events only 2
-                        
-                        //Probably i'll make this a list
-                        Text("Tomorrow")
-                            .font(
-                                Font.custom("DMSansBold", size: 16)
-                            )
-                            .padding(.top, 1)
+                    }
+                    //MARK: Right of Manage event
+                    ScrollView {
+                        VStack(alignment : .leading){
+                            //Calender Card
+                            customGraphCard(width: 344 ,
+                                            height: 285)
                             .padding([.leading, .trailing])
+                            HStack{
+                                Text("Upcoming Events")
+                                    .font(
+                                        Font.custom("DMSansBold", size: 36)
+                                    )
+                                Text("See All")
+                                    .frame(alignment: .bottom)
+                                    .font(
+                                        Font.custom("DMSans-Medium", size: 14)
+                                    ).foregroundStyle(Color("CustomButtonColor"))
+                            }.padding([.leading, .trailing])
                             
-                        HStack{
-                            customGraphCard(width: 344, height: 194)
+                            //MARK: beneath will be two closest upcoming events only 2
                             
+                            //Probably i'll make this a list
+                            Text("Tomorrow")
+                                .font(
+                                    Font.custom("DMSansBold", size: 16)
+                                )
+                                .padding(.top, 1)
                                 .padding([.leading, .trailing])
-                                .overlay(
-                                    HStack{
-                                        Image("book_cover")
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fill)
-                                            .frame(width: 112, height: 150)
-                                            .clipShape(RoundedRectangle(cornerRadius: 12))
-                        
-                                        VStack{
-                                            Text("Event Name:")
-                                                .font(
-                                                    Font.custom("DMSansBold", size: 12)
-                                                )
-                                                .foregroundStyle(.gray)
-                                            
-                                            Text("California Art Festival 2023 Dana Point 29-30")
-                                                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/).padding(.leading,45)
-                                        }//End of VStack for Event Details
-                                    }//End of HStack
-                                )
+                                
+                            HStack{
+                                customEventCard(width: 344, height: 194)
+                                
+                                    .padding([.leading, .trailing])
+                            }
+                            
+                            
+                            Text("15 JULY")
+                                .font(
+                                    Font.custom("DMSansBold", size: 16)
+                                ).padding(.top, 22)
+                                .padding([.leading, .trailing])
+                            
+                            customGraphCard(width: 344, height: 194)
+                                .padding([.leading, .trailing])
+                            
+                            //till here
+                            
                         }
-                        
-                        
-                        Text("15 JULY")
-                            .font(
-                                Font.custom("DMSansBold", size: 16)
-                            ).padding(.top, 22)
-                            .padding([.leading, .trailing])
-                        
-                        customGraphCard(width: 344, height: 194)
-                            .padding([.leading, .trailing])
-                        
-                        //till here
-                        
-                    }
-                }.padding([.leading, .trailing], 30)
-                Spacer()
+                    }.padding([.leading, .trailing], 30)
+                    Spacer()
+                }
+                HStack{
+                    BookCirculationCard(minHeight: 160, title: "All Events Listing")
+                    BookCirculationCard(minHeight: 160, title: "All Events Registration by Users")
+                }.padding(.trailing, 114)
             }
-            
-        }
-        .padding([.top, .leading], 64)
+            .padding([.top, .leading], 64)
         .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+        }
     }
 }
 
